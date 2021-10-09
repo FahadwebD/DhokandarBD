3aca1cf1a9b51ee6eaec48af5fce970289b21ad8
FahadwebD <fahadchowdhury66779@gmail.com>
2021-10-10 01:30:05 +0600

    firebase setUP

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAouii55rp5JSIm9fiCPLFEtt-SjU5pkpw",
+    authDomain: "dhokandar-bd.firebaseapp.com",
+    projectId: "dhokandar-bd",
+    storageBucket: "dhokandar-bd.appspot.com",
+    messagingSenderId: "750968802937",
+    appId: "1:750968802937:web:47fc607b264020e33d4ea8"
+  };
+
+  export default firebaseConfig;
